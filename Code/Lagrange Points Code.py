f316cb2b515436ab99cb0a23363337c4ccc451ce
mattdickers <mattdickers@gmail.com>
2020-01-22 22:26:43 +0000

    Added kagrange Points File

---
+++ Code/Lagrange Points Code.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+
+def equations(stateVecot, t):
+    x, y = stateVecot
+    #Equations go here
+    return xdot, ydot
+
+fig, ax = plt.subplots()
+ax.grid()
