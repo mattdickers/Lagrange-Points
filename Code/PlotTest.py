6813fdf9e07cd32bc1fea70b0248a16ac945010b
mattdickers <mattdickers@gmail.com>
2020-01-30 11:43:47 +0000

    Started plotting graphs

---
+++ Code/PlotTest.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+mu=0.5
+
+x = np.arange(-4,4, 0.2)
+y = (((1-mu)*(x+mu))/(abs((x+mu)**3))) + ((mu*(x+mu-1))/(abs((x+mu-1)**3)))
+#Split plot so that we dont have the lines between points and things
+
+plt.plot(x,y)
+plt.plot(x,x)
+plt.show()
