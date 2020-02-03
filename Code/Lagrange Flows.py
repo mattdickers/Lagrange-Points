839d38a0c865f8ac5ced7b9081637cf61c52270b
mattdickers <mattdickers@gmail.com>
2020-02-03 14:54:31 +0000

    Added random variable generation for plots

--- Code/Lagrange Flows.py
+++ Code/Lagrange Flows.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rc('mathtext', fontset="cm")
 from scipy.integrate import odeint
 
-# Data for plotting
+#Data for plotting
 global mu
-mu = 0.7
+mu = 0.3
+t=np.arange(0.0,20.0,0.001)  # time steps
 
-def f(state ,t):
+def motion(state ,t):
     x, y, vx, vy = state  # unpack state vector
     xdot = vx
     ydot = vy
@@ -14,35 +16,21 @@ def f(state ,t):
     vydot = -((y*(1-mu)/(((x+mu)**2 + y**2)**(3/2)))) - ((y*mu)/(((x+mu-1)**2 + y**2)**(3/2))) - y - 2*vx
     return xdot, ydot, vxdot, vydot
 
-t=np.arange(0.0,20.0,0.001)  # time steps
-
-# Note that using plt.subplots below is equivalent to using
-# fig = plt.figure and then ax = fig.add_subplot(111)
-
 fig, ax = plt.subplots()
-
 ax.grid()
-#plt.axvline(0,color='black')
-#plt.axhline(0,color='black')
 
-# flow lines from different intitial conditions
+#Plot Flow Lines:
+initial=[0.1, 0, 10, 0]
+states=odeint(motion,initial,t)
+flow1=ax.plot(states[:,0],states[:,1])[0]
 
-#more or less separatrix
-initial1=[0.1, 0, 0, 10]
-states1=odeint(f,initial1,t)
-flow1=ax.plot(states1[:,0],states1[:,1],color='royalblue')[0]
-#add_arrow(flow1,position=0.4)
-#add_arrow(flow1,position=1.75)
-
-ax.set_xlabel("$x$",fontsize=12)
-ax.set_ylabel("$y$",fontsize=12)
-
-ax.plot(3,0,'bo',markersize=15)
-ax.plot(0,2,'bo',markersize=15)
-ax.plot(0,0,'bo',fillstyle='none',markersize=15)
-ax.plot(1,1,'bo',fillstyle='none',markersize=15)
+#Plot Other Masses:
+ax.plot(-mu,0,'bo',markersize=5)
+ax.plot(1 - mu,0,'bo',markersize=5)
 
 ax.set_title(r"Flow Diagram of Object placed at N",fontsize=16)
+ax.set_xlabel("$x$",fontsize=15)
+ax.set_ylabel("$y$",fontsize=15)
 
 #fig.savefig("2d-rabbits-sheep.pdf")
 plt.show()
---
+++ Code/OrbitPlotting.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rc('mathtext', fontset="cm")
+from scipy.integrate import odeint
+
+import random
+
+def LagrangeFlowPlot(initial, mu):
+    #Data for plotting
+    #global mu
+    #mu = 0.3
+    t=np.arange(0.0,20.0,0.001)  # time steps
+
+    def motion(state ,t):
+        x, y, vx, vy = state  # unpack state vector
+        xdot = vx
+        ydot = vy
+        vxdot = -((1-mu)*(x+mu)/((x+mu)**2 + y**2)**(3/2)) - (mu*(x+mu-1))/(((x+mu-1)**2 + y**2)**(3/2)) - x + 2*vy
+        vydot = -((y*(1-mu)/(((x+mu)**2 + y**2)**(3/2)))) - ((y*mu)/(((x+mu-1)**2 + y**2)**(3/2))) - y - 2*vx
+        return xdot, ydot, vxdot, vydot
+
+    fig, ax = plt.subplots()
+    ax.grid()
+
+    #Plot Flow Lines:
+    #initial=[0.1, 0, 10, 0]
+    states=odeint(motion,initial,t)
+    flow1=ax.plot(states[:,0],states[:,1])[0]
+
+    #Plot Other Masses:
+    ax.plot(-mu,0,'bo',markersize=5)
+    ax.plot(1 - mu,0,'bo',markersize=5)
+
+    ax.set_title(r"Flow Diagram of Object placed at N",fontsize=16)
+    ax.set_xlabel("$x$",fontsize=15)
+    ax.set_ylabel("$y$",fontsize=15)
+
+    #fig.savefig("2d-rabbits-sheep.pdf")
+    plt.show()
+
+randX = random.randint(1, 100)
+randY = random.randint(1, 100)
+randVX = random.randint(0, 100)
+randVY = random.randint(0, 100)
+randMu = random.uniform(0, 1)
+
+LagrangeFlowPlot([randX, randY, randVX, randVY], randMu)
+
+print("x: ",randX)
+print("y: ",randY)
+print("vx: ",randVX)
+print("vy: ",randVY)
+print("mu: ",randMu)
