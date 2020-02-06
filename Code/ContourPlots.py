c078663f2c5d2b77f8e74b544104511e57270782
mattdickers <mattdickers@gmail.com>
2020-02-06 15:35:57 +0000

    Small Updates

--- Code/ContourPlots.py
+++ Code/ContourPlots.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 plt.rc('mathtext', fontset="cm")
 
 cmaps = ['plasma', 'rainbow', 'gist_rainbow']
+cmapType = cmaps[0]
+top = 0
+
 
 def potential(x, y, mu):
     return (0.5*(x**2 + y**2)) + ((1-mu)/np.sqrt((x + mu)**2 + y**2)) + (mu/np.sqrt((x + mu - 1)**2 + y**2))
@@ -17,9 +20,14 @@ z = potential(x, y, mu)
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-ax.contour3D(x, y, z, 50, cmap=cmaps[0])
+ax.contour3D(x, y, z, 50, cmap=cmapType)
 ax.set_xlabel(r'$x$', fontsize=15)
 ax.set_ylabel(r'$y$', fontsize=15)
-ax.set_zlabel(r'$z$', fontsize=15)
+if top == 1:
+    ax.view_init(elev=90., azim=90)
+    ax.w_zaxis.line.set_lw(0.)
+    ax.set_zticks([])
+else:
+    ax.set_zlabel(r'$z$', fontsize=15)
 ax.set_title('Plot of the Gravitational Potential for $\mu=%.2f$' % (mu,), fontsize=16)
 plt.show()
